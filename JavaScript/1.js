f573936ec1a16b0336a3d924070343e9add6a168
Anna-Bekreneva <anjabekreneva@gmail.com>
2022-07-15 18:13:12 +0300

    added js

---
+++ JavaScript/1.js
@@ -0,0 +1,6 @@
+//Try creating one of each type of comment.
+
+/* This is a
+multi-line comment */
+
+// This is an in-line comment.
---
+++ JavaScript/15.js
@@ -0,0 +1,8 @@
+// Change the code to use the ++ operator on myVar.
+
+let myVar = 87;
+
+// Only change code below this line
+myVar = ++myVar;
+
+console.log(myVar)
