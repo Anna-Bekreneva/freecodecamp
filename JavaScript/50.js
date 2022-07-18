243535f1c01541682678a2600bb4eb536f688bd2
Anna-Bekreneva <anjabekreneva@gmail.com>
2022-07-18 19:07:59 +0300

    added 10 tasks

---
+++ JavaScript/50.js
@@ -0,0 +1,8 @@
+//Create a function called functionWithArgs that accepts two arguments and outputs their sum to the dev console.
+//Call thefunction with two numbers as arguments.
+
+function functionWithArgs(a, b) {
+    console.log(a + b)
+}
+
+functionWithArgs(10, 20)
