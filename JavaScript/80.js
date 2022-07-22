49d611b0ede95d3024da046ff66ba168880bce74
Anna-Bekreneva <anjabekreneva@gmail.com>
2022-07-22 17:10:55 +0300

    added 10 tasks

---
+++ JavaScript/80.js
@@ -0,0 +1,10 @@
+//Fix the function isLess to remove the if/else statements.
+
+function isLess(a, b) {
+    // Only change code below this line
+    const res = a < b;
+    return res;
+    // Only change code above this line
+}
+
+isLess(10, 15);
