fb45245c38ac2135870f4b28843db1923167659a
Anna-Bekreneva <anjabekreneva@gmail.com>
2022-08-01 11:06:05 +0300

    test

--- JavaScript/109.js
+++
@@ -1,9 +0,0 @@
-//Use parseInt() in the convertToInteger function so it converts a binary number to an integer and returns it.
-
-function convertToInteger(str) {
-    return parseInt("10011", 10);
-}
-
-console.log(convertToInteger("10011"))
-
-console.log(parseInt("10011", 10))
